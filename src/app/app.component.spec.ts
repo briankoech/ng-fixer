00c4b6cbce450eaee6d0ab91052c572aedd249b4
Brian Koech <brnkirwa@gmail.com>
2016-12-09 11:46:41 +0300

    Add setup for basic tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,10 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpModule, Http} from '@angular/http';
 import { AppComponent } from './app.component';
+import { FixerService } from './app.service';
 
 describe('AppComponent', () => {
   beforeEach(() => {
@@ -9,6 +12,8 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [ FixerService ],
+      imports: [FormsModule, HttpModule]
     });
   });
 
@@ -18,16 +23,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
 
   it('should render title in a h1 tag', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('Fixer Currency Exchange!');
   }));
 });
