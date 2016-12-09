e3842fb9d8dc44085685c3379c261a3adc6607aa
Brian Koech <brnkirwa@gmail.com>
2016-12-09 11:29:40 +0300

    Add currency exchange edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,63 @@ import { FixerService } from './app.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public allBases: any;
+  public currencyKeys: Array<any> = [];
+  public bases: Array<any> = [];
+  public currencies: any;
+
+  public baseValue = 1;
+  public exchangeValue;
+
+  // constant Currency Rates
+  public exchangeRate: any;
+  public baseRate: any = 1;
 
   constructor(private fixerService: FixerService) {
 
   }
 
   ngOnInit() {
-    this.fixerService.listLatest();
+    this._fetchRates();
   }
+
+  public currencyChange(event) {
+    this.exchangeValue = this.currencies[event] * this.baseValue;
+
+    // change exchange value
+    this.exchangeRate = this.currencies[event];
+  }
+
+  public editCurrency() {
+    if (Number(this.exchangeValue).toString() === 'NaN') {
+      return;
+    }
+
+    console.log(this.exchangeValue);
+    this.baseValue = this.exchangeValue * this.baseRate / this.exchangeRate;
+  }
+  
+  public editBase(base) {
+
+  }
+
+  private _fetchRates(base?: string) {
+    this.fixerService.getRates(base)
+    .subscribe(data => {
+      console.log(data);
+      // build the bases array
+      this.bases = Object.keys(data.rates);
+      this.bases.unshift(data.base);
+
+      // build the currencies and currencyKeys array
+      this.currencyKeys = Object.keys(data.rates);
+      this.currencies = data.rates;
+
+      // set Exchangevalue as the first value of rates
+      this.exchangeValue = data.rates[this.currencyKeys[0]];
+
+      // set the exchange rate
+      this.exchangeRate = data.rates[this.currencyKeys[0]];
+    });
+  }
+
 }
