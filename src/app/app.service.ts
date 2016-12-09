21beb839a5f06736649e19a61f8e0d4b3afb3e5c
Brian Koech <brnkirwa@gmail.com>
2016-12-09 11:28:52 +0300

    Add base param to getRates

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,27 +2,32 @@ import { Injectable } from '@angular/core';
 import { Http, ResponseOptions, Headers} from '@angular/http';
 
 import 'rxjs/add/operator/map';
-const url = 'http://api.fixer.io/latest';
-
 
 @Injectable()
 export class FixerService {
 
-  public currencyList: any;
+  public currencyList: any = [];
   public latestRates: any;
 
   constructor(private http: Http) {}
 
   // Get all the rates base
-  public listLatest() {
-
+  public getRates(base?: string) {
+    let url;
+    if (base) {
+       url = 'http://api.fixer.io/latest?base=' + base;
+    } else {
+      url = 'http://api.fixer.io/latest';
+    }
     let headers = new Headers({'Content-Type': 'application/json'});
     let options = new ResponseOptions({headers});
 
-    this.http.get(url, options)
-    .map(res => res.json())
-    .subscribe(data => {
-      console.log(data);
-    });
+    return this.http.get(url, options)
+    .map(res => res.json());
+    // .subscribe(data => {
+    //   this.currencyList = Object.keys(data.rates);
+    //   this.currencyList.unshift(data.base);
+    //   console.log(this.currencyList);
+    // });
   }
 }
